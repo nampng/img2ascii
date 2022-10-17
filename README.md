a583c60eefd8091ebd02e60060b7c5adb3b684df
Nam <nampn344@gmail.com>
2022-10-17 16:23:32 -0500

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# img2ascii - python
+
+this is just a fun little script that will take an image, convert it to grayscale, and convert the values to ASCII characters.
+this results in a nice piece of ASCII art being output to your terminal.
---
+++ img2ascii.py
@@ -0,0 +1,33 @@
+# img2ascii
+# Take in an image file and convert it into ASCII art.
+
+from PIL import Image
+# ( .:-=+*#%@)
+
+scale = {" ": 0, ".": 55, ":":80, "-":105, "=":130, "+":155, "*":180, "#":205, "%":230, "@":255}
+
+def pixel_to_ascii(pixel: int):
+    
+    ascii = min(scale, key=lambda k: abs(scale[k] - pixel))
+
+    return ascii
+
+def main():
+    # Get image.
+    img_name = input("Filename (/img/{filename}): ")
+    with Image.open(f"./img/{img_name}") as image:
+        image = image.convert("L")
+        image = image.resize(size=(100, 100))
+        width, height = image.size
+        px = image.load()
+
+        with open('output.txt', 'w') as file:
+            for x in range(width):
+                for y in range(height):
+                    print(pixel_to_ascii(pixel=px[y, x]), end = '')
+                    # file.write(pixel_to_ascii(pixel=px[y, x]))
+                # file.write('\n')
+                print()
+
+if __name__ == "__main__":
+    main()
