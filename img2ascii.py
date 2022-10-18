d94200b18e85ee93ecc1bfbc7fc3c753391982dd
Nam <nampn344@gmail.com>
2022-10-18 15:32:03 -0500

    add cmd line args, refactor some stuff

--- img2ascii.py
+++ img2ascii.py
@@ -2,36 +2,65 @@
 # Take in an image file and convert it into ASCII art.
 
 from PIL import Image
+import argparse
 
 # The characters used to represent the gray scale images can be found here: http://paulbourke.net/dataformats/asciiart/
 # ( .:-=+*#%@)
 # ($@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. ) This just doesn't look good.
 
-scale = {}
+
+# Arguments for running in command line.
+parser = argparse.ArgumentParser(description="Turn an image into ASCII art.")
+parser.add_argument(
+    "scale",
+    default="short",
+    const="short",
+    nargs="?",
+    type=str,
+    choices=["short", "long"],
+    help="Scale to be used. Short provides a less detailed image, as opposed to long.",
+)
+parser.add_argument(
+    "--flip",
+    action="store_true",
+    help="Flips scale when converting grayscale to ASCII.",
+)
 
 
-def create_scale(flip_scale: bool = False):
+def short_scale(flip: bool = False):
+    scale = {}
     chars = " .:-=+*#%@"
     val = 255  # white = ' ', black = '@'
-    step = -255 / len(chars)
+    step = -(255 // len(chars))
 
-    if flip_scale:  # This will make white == "@" and black == " "
+    if flip:  # This will make white == "@" and black == " "
         val = 0
-        step = -step
+        step = abs(step)
 
     for char in chars:
         scale[char] = val
         val += step
 
+    print(scale)
 
-def pixel_to_ascii(pixel: int):
+    return scale
 
+
+def create_scale(scale_type: str = "short", flip: bool = False):
+    if scale_type == "short":
+        return short_scale(flip)
+    elif scale_type == "long":
+        return {}
+
+
+def pixel_to_ascii(pixel: int, scale):
     char = min(scale, key=lambda k: abs(scale[k] - pixel))
-
     return char
 
 
-def main():
+def convert(scale_type: str = "short", flip: bool = False):
+    # Create scale
+    scale = create_scale(scale_type, flip)
     # Get image.
     img_name = input("Filename (/img/{filename}): ")
     with Image.open(f"./img/{img_name}") as image:
@@ -42,9 +71,9 @@ def main():
         px = image.load()
 
         with open("output.txt", "w") as file:
-            for y in range(height):
+            for y in range(0, height, 2):
                 for x in range(width):
-                    char = pixel_to_ascii(pixel=px[x, y])
+                    char = pixel_to_ascii(pixel=px[x, y], scale=scale)
                     print(char, end="")
                     file.write(char)
                 print()
@@ -52,5 +81,6 @@ def main():
 
 
 if __name__ == "__main__":
-    create_scale(flip_scale=False)
-    main()
+    args = parser.parse_args()
+    print(args.scale, args.flip)
+    convert(args.scale, args.flip)
